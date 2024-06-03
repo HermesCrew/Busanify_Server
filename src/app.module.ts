d4f8731e46f58147de7608684d9861483eae5a0e
Lee Inho <inhoking01@gmail.com>
2024-06-03 13:02:30 +0900

    feat. nestjs init

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './config/typeorm.config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(typeORMConfig)],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
