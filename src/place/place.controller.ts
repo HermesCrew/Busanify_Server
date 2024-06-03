6f9733a2ccb8f7debf68d8b5f0cdb976b541c66e
Lee Inho <inhoking01@gmail.com>
2024-06-04 01:44:35 +0900

    feat. add get db operation

--- src/place/place.controller.ts
+++ src/place/place.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { PlaceService } from 'src/place/place.service';
 import { OpenAPIService } from './openapi.service';
 
 @Controller('place')
 export class PlaceController {
-  constructor(private readonly openAPIService: OpenAPIService) {}
+  constructor(
+    private readonly openAPIService: OpenAPIService,
+    private readonly placeService: PlaceService,
+  ) {}
 
   @Get('/save')
   async fetchAndSave() {
     this.openAPIService.saveFetchData();
+  }
+
+  @Get('type/:value')
+  async readByType(@Param('value') type: string) {
+    return await this.placeService.findByType(type);
+  }
+
+  @Get('title')
+  async readByTitle(@Query('value') title: string) {
+    return await this.placeService.findByTitle('eng', title);
   }
 }
--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -6,4 +6,24 @@ import { PlaceEntity } from 'src/entity/place.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
-export class PlaceService {}
+export class PlaceService {
+  constructor(
+    @InjectRepository(PlaceEntity)
+    private placeRepository: Repository<PlaceEntity>,
+  ) {}
+
+  async findByTitle(lang: string, title: string): Promise<PlaceEntity[]> {
+    if (lang === 'eng') {
+      return await this.placeRepository
+        .createQueryBuilder('place')
+        .where('place.titleEng LIKE :title', { title: `%${title}%` })
+        .getMany();
+    }
+  }
+  async findByType(type: string): Promise<PlaceEntity[]> {
+    return await this.placeRepository
+      .createQueryBuilder('place')
+      .where('place.type = :type', { type: type })
+      .getMany();
+  }
+}
